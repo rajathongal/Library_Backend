7d29b970e31770897c28edf96d7b0b780b8a085c
rajathongal <reachme.rajathongal@gmail.com>
2021-06-28 17:17:27 +0530

    authors list api added

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const { addBook, bookView, addBookCoverPage } = require("../controllers/addBook");
 const { booksList } = require("../controllers/booksList");
+const { authorList } = require("../controllers/authorList");
 
 router.post("/addbook", addBook);
 router.get("/booksList", booksList);
 router.post("/getBook", bookView);
 router.post("/addCoverPage", addBookCoverPage);
+router.get("/authorList", authorList);
 
 module.exports = router;
