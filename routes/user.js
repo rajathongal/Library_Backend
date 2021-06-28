3e14f1ed2b9fc23fc4cf2bbfb2096d568564e542
rajathongal <reachme.rajathongal@gmail.com>
2021-06-28 12:47:41 +0530

    list API added

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { addBook } = require("../controllers/addBook");
+const { booksList } = require("../controllers/booksList");
 
 router.post("/addbook", addBook);
+router.get("/booksList", booksList);
 
 module.exports = router;
